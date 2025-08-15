cb391c2f6d6ca8f5666bbd0c3a2b2b8d246f98ee
BettyCheng <bettycheng0921@gmail.com>
2025-08-15 15:40:14 -0400

    add log

--- utils/engine.py
+++ utils/engine.py
@@ -27,6 +27,7 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
         lr_scheduler = utils.warmup_lr_scheduler(optimizer, warmup_iters, warmup_factor)
 
     for images, targets in metric_logger.log_every(data_loader, print_freq, header):
+        print(f"images: {len(images)}, targets: {len(targets)}")
         images = list(image.to(device) for image in images)
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
