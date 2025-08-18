ce01461444ad5babc3e94f16f691d4131a759c17
BettyCheng <bettycheng0921@gmail.com>
2025-08-18 10:22:11 -0400

    save path every epoch

--- finetune_maskrcnn.py
+++ finetune_maskrcnn.py
@@ -67,6 +67,7 @@ if __name__ == "__main__":
         train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=1000)
         lr_scheduler.step()
         evaluate(model, data_loader_val, device=device)
+        torch.save(model.state_dict(), f"nuimages_maskrcnn_epoch{epoch}.pth")
 
     torch.save(model.state_dict(), "nuimages_maskrcnn.pth")
 
